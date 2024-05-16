defba35144163efa71a92b083a3a84321dd93b2d
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2024-05-16 22:01:48 +0300

    Update main()

--- blackjack.py
+++ blackjack.py
@@ -31,6 +31,72 @@ def main():
             print('Thanks for playing!')
             sys.exit()
 
+        print('Money:', money)
+        bet = get_bet(money)
+
+        deck = get_deck()
+        dealer_hand = [deck.pop(), deck.pop()]
+        player_hand = [deck.pop(), deck.pop()]
+
+        print('Bet:', bet)
+        while True:
+            display_hands(player_hand, dealer_hand, False)
+            print()
+
+            if get_hand_value(player_hand) > 21:
+                break
+
+            move = get_move(player_hand, money - bet)
+
+            if move == 'D':
+                additional_bet = get_bet(min(bet, (money - bet)))
+                bet += additional_bet
+                print(f'Bet increased to {bet}')
+                print('Bet', bet)
+
+            if move in ('H', 'D'):
+                new_card = deck.pop()
+                rank, suit = new_card
+                print(f'You drew a {rank} of {suit}.')
+                player_hand.append(new_card)
+
+                if get_hand_value(player_hand) > 21:
+                    continue
+
+            if move in ('S', 'D'):
+                break
+
+        if get_hand_value(player_hand) <= 21:
+            while get_hand_value(dealer_hand) < 17:
+                print('Dealer hits...')
+                dealer_hand.append(deck.pop())
+                display_hands(player_hand, dealer_hand, False)
+
+                if get_hand_value(dealer_hand) > 21:
+                    break
+                input('Press Enter to continue...')
+                print('\n\n')
+
+        display_hands(player_hand, dealer_hand, False)
+
+        player_value = get_hand_value(player_hand)
+        dealer_value = get_hand_value(dealer_hand)
+
+        if dealer_value > 21:
+            print(f'Dealer busts! You win ${bet}')
+            money += bet
+        elif (player_value > 21) or (player_value < dealer_value):
+            print('You lost!')
+            money -= bet
+        elif player_value > dealer_value:
+            print(f'You won ${bet}')
+            money += bet
+        elif player_value == dealer_value:
+            print('It\'s a tie, the bet is returned to you.')
+
+        input('Press Enter to continue...')
+        print('\n\n')
+
 
 def get_bet(max_bet):
     """Ask player how much to bet for this round."""
